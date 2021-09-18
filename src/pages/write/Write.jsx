23c969f248a8165bfcec1300e1d679ac8bf5d860
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-09-18 09:16:06 +0900

    Created New Post Component

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,5 +1,34 @@
 import './write.css'
 
 export default function Write() {
-  return <div className='write'>WRITE</div>
+  return (
+    <div className='write'>
+      <img
+        src='https://images.pexels.com/photos/620337/pexels-photo-620337.jpeg?cs=srgb&dl=pexels-tobi-620337.jpg&fm=jpg'
+        alt=''
+        className='writeImg'
+      />
+      <form className='writeForm'>
+        <div className='writeFormGroup'>
+          <label htmlFor='fileInput'>
+            <i className='writeIcon fas fa-plus'></i>
+          </label>
+          <input type='file' id='fileInput' />
+          <input
+            type='text'
+            placeholder='Title'
+            className='writeInput'
+            autoFocus={true}
+          />
+        </div>
+        <div className='writeFormGroup'>
+          <textarea
+            placeholder='Tell your story...'
+            type='text'
+            className='writeInput writeText'></textarea>
+        </div>
+        <button className='writeSubmit'>Publish</button>
+      </form>
+    </div>
+  )
 }
