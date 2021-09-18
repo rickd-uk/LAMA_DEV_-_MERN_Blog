19bdaf5b2970b7cdab5a7233c9b75adfd6be2580
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-09-18 11:17:06 +0900

    Added routing & updated links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className='header'>
       <div className='headerTitles'>
-        <span className='headerTitleSm'>React & Node</span>
+        <span className='headerTitleSm'>All Things</span>
         <span className='headerTitleLg'>Blog</span>
       </div>
       <img
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom'
 import './topbar.css'
 
 export default function TopBar() {
+  const user = false
   return (
     <div className='top'>
       <div className='topLeft'>
@@ -12,20 +14,51 @@ export default function TopBar() {
 
       <div className='topCenter'>
         <ul className='topList'>
-          <li className='topListItem'>HOME</li>
-          <li className='topListItem'>ABOUT</li>
-          <li className='topListItem'>CONTACT</li>
-          <li className='topListItem'>WRITE</li>
-          <li className='topListItem'>LOGOUT</li>
+          <li className='topListItem'>
+            <Link to='/' className='link'>
+              HOME
+            </Link>
+          </li>
+          <li className='topListItem'>
+            <Link to='/about' className='link'>
+              ABOUT
+            </Link>
+          </li>
+          <li className='topListItem'>
+            <Link to='/contact' className='link'>
+              CONTACT
+            </Link>
+          </li>
+          <li className='topListItem'>
+            <Link to='/write' className='link'>
+              WRITE
+            </Link>
+          </li>
+          <li className='topListItem'>{user && 'LOGOUT'}</li>
         </ul>
       </div>
 
       <div className='topRight'>
-        <img
-          className='topImg'
-          src='https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
-          alt=''
-        />
+        {user ? (
+          <img
+            className='topImg'
+            src='https://images.unsplash.com/photo-1432821596592-e2c18b78144f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+            alt=''
+          />
+        ) : (
+          <ul className='topList'>
+            <li className='topListItem'>
+              <Link className='link' to='/login'>
+                LOGIN
+              </Link>
+            </li>
+            <li className='topListItem'>
+              <Link className='link' to='/register'>
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
         <i className='topSearchIcon fas fa-search'></i>
       </div>
     </div>
